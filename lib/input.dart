6db125145dd5200d4cd7ae675c144a56dd5b716b
MrJoji23 <juaanmaarin23@outlook.com>
2026-05-28 19:02:46 -0500

    se ingresa el boton y para validar

--- lib/input.dart
+++ lib/input.dart
@@ -10,8 +10,8 @@ class Inputs extends StatelessWidget {
       home: MyFormulario(),
     );
   }
-
 }
+
 class MyFormulario extends StatefulWidget {
   const MyFormulario({super.key});
 
@@ -20,28 +20,30 @@ class MyFormulario extends StatefulWidget {
 }
 
 class _MyFormularioState extends State<MyFormulario> {
-//KEY formulario
-  final _formKey =GlobalKey<FormState>();
-//Controllers
+  //KEY formulario
+  final _formKey = GlobalKey<FormState>();
+  //Controllers
   final nombreController = TextEditingController();
   final apellidoController = TextEditingController();
   final phoneController = TextEditingController();
-  
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
-//variables del sistema
-String rol = 'Usuario';
-DateTime? fechaNacimiento; // el ? es para que el sistema sepa que puede estar en vacio 
-
+  //variables del sistema
+  String rol = 'Usuario';
+  DateTime?
+  fechaNacimiento; // el ? es para que el sistema sepa que puede estar en vacio
+  bool acceptaTerminos = false;
+  bool notificaciones = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Inputs')),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Input form')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextField(
@@ -63,71 +65,132 @@ DateTime? fechaNacimiento; // el ? es para que el sistema sepa que puede estar e
               ),
               SizedBox(height: 16),
               TextFormField(
-                decoration: InputDecoration(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
                   labelText: 'Correo Electronico',
                   hintText: 'Escribe el correo',
                   border: OutlineInputBorder(),
                   prefixIcon: Icon(Icons.email),
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty || !value.contains('@')) {
+                    return 'Por favor, ingresa tu correo';
+                  } else if (!RegExp(
+                    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                  ).hasMatch(value)) {
+                    return 'Por favor, ingresa un correo valido';
+                  }
+                  return null;
+                },
               ),
-              
+
               SizedBox(height: 16),
               DropdownButtonFormField(
                 value: rol,
-                decoration:  const InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'Rol',
                   border: OutlineInputBorder(),
                 ),
                 items: const [
-                  DropdownMenuItem(
-                    value: 'Usuario',
-                    child: Text('Usuario'),
-                  ),
+                  DropdownMenuItem(value: 'Usuario', child: Text('Usuario')),
                   DropdownMenuItem(
                     value: 'Administrador',
                     child: Text('admin'),
                   ),
-                  DropdownMenuItem(
-                    value: 'Cliente',
-                    child: Text('Cliente'),
-                  ),
+                  DropdownMenuItem(value: 'Cliente', child: Text('Cliente')),
                 ],
                 onChanged: (value) {
                   setState(() {
-                    rol =value!;
+                    rol = value!;
                   });
-                }
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length < 6) {
+                    return 'La contraseña debe tener al menos 6 caracteres';
+                  }
+                  return null;
+                },
               ),
               SizedBox(height: 20),
-            
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton.icon(
-                icon: const Icon(Icons.calendar_month),
-                label: Text(
-                  fechaNacimiento == null
-                  ? 'Seleccionar fecha de nacimiento'
-                  : 'Fecha: ${fechaNacimiento!.day}/${fechaNacimiento!.month}/${fechaNacimiento!.year}',
+
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    icon: const Icon(Icons.calendar_month),
+                    label: Text(
+                      fechaNacimiento == null
+                          ? 'Seleccionar fecha de nacimiento'
+                          : 'Fecha: ${fechaNacimiento!.day}/${fechaNacimiento!.month}/${fechaNacimiento!.year}',
+                    ),
+                    onPressed: () async {
+                      DateTime? fecha = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1950),
+                        lastDate: DateTime(2027),
+                        initialEntryMode: DatePickerEntryMode.input,
+                      );
+                      if (fecha != null) {
+                        setState(() {
+                          fechaNacimiento = fecha;
+                        });
+                      }
+                    },
                   ),
-                  onPressed: () async{
-                    DateTime? fecha = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1950),
-                      lastDate: DateTime(2027),
-                      initialEntryMode: DatePickerEntryMode.input,
-                    );
-                    if(fecha != null){
-                      setState(() {
-                        fechaNacimiento = fecha;
-                      });
+                ),
+              ),
+              SizedBox(height: 20),
+              CheckboxListTile(
+                title: const Text('Acepto termino y condiciones'),
+                value: acceptaTerminos,
+                onChanged: (value) {
+                  setState(() {
+                    acceptaTerminos = value!;
+                  });
+                },
+              ),
+
+              SizedBox(height: 20),
+              SwitchListTile(
+                title: const Text('Recibir notis'),
+                value: notificaciones,
+                onChanged: (value) {
+                  setState(() {
+                    notificaciones = value;
+                  });
+                },
+              ),
+
+              SizedBox(height: 20),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate() && acceptaTerminos) {
+                      print('formulario registro melo');
+                    } else {
+                      print("Por favor, acepeta terminos unu");
                     }
                   },
-              )
-            ) 
-
+                  child: const Text('Registrar'),
+                ),
+              ),
             ],
-          ),
           ),
         ),
       ),
