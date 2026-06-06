f910fdabecb5330ed609fe827481ce078c5c9436
MrJoji23 <juaanmaarin23@outlook.com>
2026-06-05 20:37:27 -0500

    se termina

--- lib/cal.dart
+++ lib/cal.dart
@@ -8,7 +8,12 @@ class Calculadora extends StatefulWidget {
 }
 
 class _CalculadoraState extends State<Calculadora> {
-  String operacion = '';
+  String operacion = "";
+  String resultado = "0";
+  String numeroActual = "";
+  List<String> elementos = [];
+  List<String> lista = [];
+  int i = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,39 +30,132 @@ class _CalculadoraState extends State<Calculadora> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.end,
-                  children: const [
+                  children: [
                     Text(
                       operacion,
-                      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 50),
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 50,
+                      ),
                     ),
                     Text(
-                      '12',
-                      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 30),
+                      resultado,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 30,
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-            Row(
-                  children: [boton('7'), boton('8'), boton('9'), boton('/')]),
-            Row(
-                  children: [boton('4'), boton('5'), boton('6'), boton('*')]),
-            Row( 
-                  children: [boton('1'), boton('2'), boton('3'), boton('-')]),
-            Row(
-                  children: [boton('0'), boton('.'), boton('='), boton('+')]),
+            Row(children: [boton('C'), boton('CE'), boton('%'), boton('←')]),
+            Row(children: [boton('7'), boton('8'), boton('9'), boton('/')]),
+            Row(children: [boton('4'), boton('5'), boton('6'), boton('*')]),
+            Row(children: [boton('1'), boton('2'), boton('3'), boton('-')]),
+            Row(children: [boton('0'), boton('.'), boton('='), boton('+')]),
           ],
         ),
       ),
     );
   }
 
-  Widget boton (String texto) {
+  void actionButton(String valor) {
+    //print(valor);
+    setState(() {
+      switch (valor) {
+        case 'C ':
+          operacion = "";
+          resultado = "";
+          break;
+        case 'CE':
+          operacion = "";
+          break;
+        case '←':
+          if (operacion.isNotEmpty) {
+            operacion = operacion.substring(0, operacion.length - 1);
+          }
+          break;
+
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+          if (numeroActual.isNotEmpty) {
+            elementos.add(numeroActual);
+            elementos.add(valor);
+            numeroActual = "";
+            operacion += valor;
+          }
+          print(elementos);
+
+          break;
+        case "=":
+          if (numeroActual.isNotEmpty) {
+            elementos.add(numeroActual);
+          }
+
+          lista = List.from(elementos);
+          bool hayOperadores = true;
+
+          while (hayOperadores) {
+            hayOperadores = false;
+
+            for (i = 0; i < lista.length; i++) {
+              if (lista[i] == '*' || lista[i] == '/') {
+                double num1 = double.parse(lista[i - 1]);
+                double num2 = double.parse(lista[i + 1]);
+                double r = 0;
+
+                if (lista[i] == "*") {
+                  r = num1 * num2;
+                } else {
+                  r = num1 / num2;
+                }
+
+                lista.replaceRange(i - 1, i + 2, [r.toString()]);
+                hayOperadores = true;
+
+                break;
+              }
+            }
+          }
+          double total = double.parse(lista[0]);
+          for(int i = 1; i<lista.length; i+=2){
+            String operdor = lista[i];
+
+            double numero = double.parse(lista[i+1]);
+
+            switch(operdor){
+              case "+":
+              total += numero;
+              break;
+              case "-":
+              total -= numero;
+              break;
+            }
+          }
+          resultado = total.toString();
+          elementos.clear();
+          numeroActual = resultado;
+          break;
+
+        default:
+          operacion += valor;
+          numeroActual += valor;
+          break;
+      }
+    });
+  }
+
+  Widget boton(String texto) {
     return Expanded(
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          actionButton(texto);
+        },
         child: Text(texto),
       ),
     );
-}
+  }
 }
